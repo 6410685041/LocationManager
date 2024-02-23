d5125150eefbef9b9dce9ec7108dee342a54a13c
6410685041 <6410685041@student.tu.ac.th>
2024-02-23 10:09:27 +0700

    Initial Commit

---
+++ LocationManager/LocationManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationManagerApp.swift
+//  LocationManager
+//
+//  Created by นางสาวชลิศา ธรรมราช on 23/2/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct LocationManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
